63f49b3397eac2c017a7ce35d0a136877b133931
21chanas3 <adenchan@gmail.com>
2022-07-08 22:44:02 -0700

    First Commit - Created Cell Class

---
+++ src/Cell.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Cell {
+    private int strength;
+    private int x,y;
+    private int id;
+    private HashSet<String> signalChemicalsEmitting;
+
+    public Cell(int strength, int x, int y, int id) {
+        setStrength(strength);
+        setX(x);
+        setY(y);
+        setId(id);
+    }
+
+    public void interactNeighbour(ArrayList<Cell> neighbours) {
+
+    }
+
+    //Setters & Getters
+    public void setStrength(int strength) {
+        this.strength = Math.max(strength, 0);
+    }
+
+    public void setX(int x) {
+        this.x = Math.max(x,0);
+    }
+
+    public void setY(int y) {
+        this.y = Math.max(y,0);
+    }
+
+    public void setId(int id) { //probably needs something to check if ID already exists and if yes reject
+        this.id = Math.max(id, 0);
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
